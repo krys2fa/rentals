3d07469c3415285f07e73118a733b00aabb196a5
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-09-02 11:46:55 +0000

    Implement logout endpoint

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -65,4 +65,6 @@ export const login = async (req, res) => {
     }
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+    res.clearCookie("token").status(200).json({message: "Logout Successful"});
+};
