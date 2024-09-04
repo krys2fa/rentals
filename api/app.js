57f7c0382e0d92a209e4f2526140e0f603c096f3
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-09-04 10:48:35 +0000

    Add fix for CORs errors and implement UI and chat template

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
@@ -7,10 +8,17 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
+    credentials: true,
+  })
+);
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
-  console.log("Server is running!");
+  console.log("Server is running on port 8800!");
 });
