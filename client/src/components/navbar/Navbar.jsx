0b19b15fb8548f1467ae94e3b82d2de17bb3fd68
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-09-04 11:02:03 +0000

    Update names

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -18,8 +18,8 @@ function Navbar() {
     <nav>
       <div className="left">
         <a href="/" className="logo">
-          <img src="/logo.png" alt="" />
-          <span>LamaEstate</span>
+          {/* <img src="/logo.png" alt="" /> */}
+          <span>Project Direct</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
