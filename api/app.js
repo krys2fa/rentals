c9286ab5fa7f15e85c36be81e2e3cb955cfa0091
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-09-05 19:49:17 +0000

    Temporary fix for Render environment variable

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || 'https://propertydirect.netlify.app/',
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
