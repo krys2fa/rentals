177c41da76331c253bccc7527eb332a476662df9
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-09-05 19:03:48 +0000

    temporary test

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log(process.env.REACT_APP_API_URL)
+
 const apiRequest = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL ||  'https://rentals-5dps.onrender.com',
   withCredentials: true,
 });
 
