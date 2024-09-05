77a42daa0d477a0a78afe5a90370064cf1b7d671
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-09-05 22:18:12 +0000

    Temporary fix for cors error

--- api/app.js
+++ api/app.js
@@ -8,14 +8,30 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'https://propertydirect.netlify.app/',
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || 'https://propertydirect.netlify.app/',
+//     methods: "GET,POST,PUT,DELETE",
+//     allowedHeaders: "Content-Type,Authorization",
+//     credentials: true,
+//   })
+// );
+
+const allowedOrigins = [process.env.CLIENT_URL, 'https://propertydirect.netlify.app'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true, 
+}));
+
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
